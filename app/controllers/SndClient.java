ccceb975b81eb60fe710a2e687024d56bab726aa
Rickard Larsson <rickard.sigters@gmail.com>
2014-04-07 13:12:08 +0200

    Lägger till SND koppling,

---
+++ app/controllers/SndClient.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class SndClient extends Controller {
+
+	
+	  public static void index () {
+		  System.out.println("Sndclient.index");
+	        render();
+	    }
+	
+}
